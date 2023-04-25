bac2b75f22409e7a028c41ea487ccf33989d60b9
amarko1 <amarko1@racunarstvo.hr>
2023-04-25 19:50:33 +0200

    Sve spremno za java script

---
+++ Kontakt/kontakt.js
@@ -0,0 +1,11 @@
+function ShowForm(){
+  var forma = document.getElementById("bgkontakt");
+  forma.style.display = "block"; 
+  document.body.style.overflow = "hidden";
+}
+
+function HideForm(){
+  var forma = document.getElementById("bgkontakt");
+  forma.style.display = "none"; 
+  document.body.style.overflow = "visible";
+}
